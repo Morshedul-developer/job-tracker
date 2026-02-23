a5687e5f4fa5d66faa491d38f4ad2995c06a4b3a
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-02-23 17:30:15 +0600

    made the toggling functionality for the avalability buttons

--- script.js
+++ script.js
@@ -6,12 +6,29 @@ const interviewJobs = document.getElementById("interview");
 const rejectedJobs = document.getElementById("rejected");
 const availableJobs = document.getElementById("available-jobs");
 
+const allBtn = document.getElementById("btn-all");
+const interviewBtn = document.getElementById("btn-interview");
+const rejectedBtn = document.getElementById("btn-rejected");
+
+// count the jobs
 function jobCounts() {
     const totalJobs = document.getElementById('total');
     totalJobs.innerText = jobList.children.length;
     availableJobs.innerText = jobList.children.length;
-
-    interviewJobs.innerText = interview.length;
-    rejectedJobs.innerText = rejected.length;
 }
 jobCounts();
+
+// toggling the buttons
+function toggleButtons(id) {
+    allBtn.classList.add('btn-outline-gray-500')
+    interviewBtn.classList.add('btn-outline-gray-500')
+    rejectedBtn.classList.add('btn-outline-gray-500')
+
+    allBtn.classList.remove('btn-primary')
+    interviewBtn.classList.remove('btn-primary')
+    rejectedBtn.classList.remove('btn-primary')
+
+    const selected = document.getElementById(id);
+    selected.classList.add('btn-primary');
+    selected.classList.remove('btn-outline-gray-500')
+}
